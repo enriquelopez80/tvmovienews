32713a45d86c8ec3bb10391dbc21aad9f77ccd92
Rico <enriquelopez80@gmail.com>
2018-09-22 20:09:49 -0700

    mongo

--- server.js
+++ server.js
@@ -33,12 +33,13 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect("mongodb://localhost/mongoHeadlines", {
+    useNewUrlParser: true
+});
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
+
 
 //routes
 
